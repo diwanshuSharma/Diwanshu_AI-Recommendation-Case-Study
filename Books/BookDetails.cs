38aafc2c6dc6e537379b8dd96e922e4831bb7470
kartik kumar <thisiskartik14@gmail.com>
2020-12-16 14:14:33 +0530

    small changes

--- Books/BookDetails.cs
+++ Books/BookDetails.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace BookUserRatingLib
 {
-    public class BookDetails 
+    public class BookDetails
     {
         public List<Book> Books { get; set; } = new List<Book>();
         public List<BookUserRating> BookUserRatings { get; set; } = new List<BookUserRating>();
         public List<User> User { get; set; } = new List<User>();
-
     }
 }
