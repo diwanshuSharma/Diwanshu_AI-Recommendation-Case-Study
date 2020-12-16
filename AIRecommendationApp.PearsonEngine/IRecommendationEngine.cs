93c319c3d96acac7df59b6db52787a1feda56194
Kartik kumar <thisiskartik14@gmail.com>
2020-12-16 10:03:56 +0530

    Add project files.

---
+++ AIRecommendationApp.PearsonEngine/IRecommendationEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIRecommendationApp.PearsonEngine
+{
+    public interface IRecommendationEngine
+    {
+        double GetCorellation(int[] a1, int[] a2);
+    }
+}
