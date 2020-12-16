d2a911e1e88ea2ce64db1494c899cda1a84006d9
kartik kumar <thisiskartik14@gmail.com>
2020-12-16 13:57:53 +0530

    initilized

--- Books/BookDetails.cs
+++ Books/BookDetails.cs
@@ -8,9 +8,9 @@ namespace BookUserRatingLib
 {
     public class BookDetails 
     {
-        public List<Book> Books { get; set; }
-        public List<BookUserRating> BookUserRatings { get; set; }
-        public List<User> User { get; set; }
+        public List<Book> Books { get; set; } = new List<Book>();
+        public List<BookUserRating> BookUserRatings { get; set; } = new List<BookUserRating>();
+        public List<User> User { get; set; } = new List<User>();
 
     }
 }
