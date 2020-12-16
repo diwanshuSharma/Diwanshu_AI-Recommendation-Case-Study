8a1040bf79aba44f471e75b62ac1354de8446aac
kartik kumar <thisiskartik14@gmail.com>
2020-12-16 11:55:38 +0530

    Data Class addedd

---
+++ Books/BXBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    public class BXBook
+    {
+        public string ISBN { get; set; }
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+        public int YearOfPublication { get; set; }
+        public string Publisher { get; set; }
+        public string ImageURLS { get; set; }
+        public string ImageURLM { get; set; }
+        public string ImageURLL { get; set; }
+    }
+}
+
+/*"ISBN"; "Book-Title"; "Book-Author"; "Year-Of-Publication"; "Publisher"; "Image-URL-S"; "Image-URL-M"; "Image-URL-L"
+"0195153448"; "Classical Mythology"; "Mark P. O. Morford"; "2002"; "Oxford University Press"; "http://images.amazon.com/images/P/0195153448.01.THUMBZZZ.jpg"; "http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg"; "http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg"*/
---
+++ Books/BXBookRating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    public class BXBookRating
+    {
+        public int UserID { get; set; }
+        public string ISBN { get; set; }
+        public int BookRating { get; set; }
+
+    }
+    
+}
