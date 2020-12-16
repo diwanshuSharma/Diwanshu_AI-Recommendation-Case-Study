ddab0dacbc96d4dfaed16f3c4d1f2c20ef450f3a
kartik kumar <thisiskartik14@gmail.com>
2020-12-16 11:35:16 +0530

    MathNetPersonRecommendation engine added

---
+++ AIRecommendationApp.PearsonEngine/MathNetPearsonRecommendationEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Statistics;
+
+namespace AIRecommendationApp.PearsonEngine
+{
+    class MathNetPearsonRecommendationEngine : IRecommendationEngine
+    {
+        public double GetCorrelation(List<int> baselist, List<int> otherlist)
+        {
+
+            return Correlation.Pearson(baselist.Select<int, double>(i => i).ToList(), otherlist.ConvertAll<double>(i=> i));
+
+        }
+    }
+}
